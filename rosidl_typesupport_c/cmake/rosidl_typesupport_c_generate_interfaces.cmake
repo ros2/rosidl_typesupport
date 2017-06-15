5fc8e912d12beffc0f7a5484add37558cc8ddc62
Mikael Arguedas <mikael@osrfoundation.org>
2017-06-15 13:53:02 -0700

    use CMAKE_X_STANDARD and check compiler rather than platform
    remove todo, compiler come from redefinition of typedef

--- rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
+++ rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
@@ -101,9 +101,14 @@ if(WIN32)
   target_compile_definitions(${rosidl_generate_interfaces_TARGET}${_target_suffix}
     PRIVATE "ROSIDL_TYPESUPPORT_C_BUILDING_DLL_${PROJECT_NAME}")
 endif()
-if(NOT WIN32)
+
+# Default to C++14
+if(NOT CMAKE_CXX_STANDARD)
+  set(CMAKE_CXX_STANDARD 14)
+endif()
+if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix}
-    PROPERTIES COMPILE_FLAGS "-std=c++14 -Wall -Wextra -Wpedantic")
+    PROPERTIES COMPILE_FLAGS "-Wall -Wextra -Wpedantic")
 endif()
 target_include_directories(${rosidl_generate_interfaces_TARGET}${_target_suffix}
   PUBLIC
