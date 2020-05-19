dea9825fb7af51a74007ff5999aec544ae4c921e
brawner <brawner@gmail.com>
2020-05-19 16:24:11 -0700

    Add tests for type_support functions (#63)
    * Add tests for type_support functions
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Windows fixes
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Avoid installing the test libs and fix the cpp tests
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>
    
    Co-authored-by: Scott K Logan <logans@cottsay.net>

---
+++ rosidl_typesupport_cpp/test/test_type_support.cpp
@@ -0,0 +1,37 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Provide a symbol so they can be checked with get_typesupport_handle_function()
+
+#include "rosidl_typesupport_cpp/visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// If ROSIDL_TYPESUPPORT_CPP_PUBLIC is used, it selects dllimport instead of dllexport, but the
+// function still needs to be defined separately. Windows has gotten picky with its compiler
+// warnings recently.
+#if defined _WIN32 || defined __CYGWIN__
+__declspec(dllexport) void test_type_support();
+#else
+void test_type_support();
+#endif
+
+void test_type_support() {}
+
+#ifdef __cplusplus
+}
+#endif
