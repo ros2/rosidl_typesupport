82401265de32e0130c7233f1f524e135bfb18ae2
Michel Hidalgo <michel@ekumenlabs.com>
2021-06-29 12:29:02 -0300

    Clean up dependency relationships
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
+++ rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
@@ -111,7 +111,6 @@ if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 endif()
 target_include_directories(${rosidl_generate_interfaces_TARGET}${_target_suffix}
   PUBLIC
-  "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_c>"
   "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/rosidl_typesupport_c>"
   "$<INSTALL_INTERFACE:include>"
 )
@@ -143,14 +142,13 @@ foreach(_pkg_name ${rosidl_generate_interfaces_DEPENDENCY_PACKAGE_NAMES})
     ament_export_dependencies(${_pkg_name})
   endif()
 endforeach()
+target_link_libraries(${rosidl_generate_interfaces_TARGET}${_target_suffix}
+  ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
+)
 
 add_dependencies(
   ${rosidl_generate_interfaces_TARGET}
   ${rosidl_generate_interfaces_TARGET}${_target_suffix}
-)
-add_dependencies(
-  ${rosidl_generate_interfaces_TARGET}${_target_suffix}
-  ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
 )
 
 if(NOT rosidl_generate_interfaces_SKIP_INSTALL)
