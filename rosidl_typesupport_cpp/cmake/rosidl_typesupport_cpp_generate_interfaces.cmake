cb32f3a6cf6fddcb439e470b5769c3fd92f554fc
Michel Hidalgo <michel@ekumenlabs.com>
2021-08-04 10:03:14 -0300

    Revert "Bundle and ensure the exportation of rosidl generated targets (#113)"
    This reverts commit 78fadb5243ff6e0cab7661e18d9531c5c77dc8eb.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- rosidl_typesupport_cpp/cmake/rosidl_typesupport_cpp_generate_interfaces.cmake
+++ rosidl_typesupport_cpp/cmake/rosidl_typesupport_cpp_generate_interfaces.cmake
@@ -144,12 +144,13 @@ add_dependencies(
 if(NOT rosidl_generate_interfaces_SKIP_INSTALL)
   install(
     TARGETS ${rosidl_generate_interfaces_TARGET}${_target_suffix}
-    EXPORT ${rosidl_generate_interfaces_TARGET}
+    EXPORT ${rosidl_generate_interfaces_TARGET}${_target_suffix}
     ARCHIVE DESTINATION lib
     LIBRARY DESTINATION lib
     RUNTIME DESTINATION bin
   )
   ament_export_libraries(${rosidl_generate_interfaces_TARGET}${_target_suffix})
+  ament_export_targets(${rosidl_generate_interfaces_TARGET}${_target_suffix})
 endif()
 
 if(BUILD_TESTING AND rosidl_generate_interfaces_ADD_LINTER_TESTS)
