56e12b9b88719f976b839e5716dbd14c9a13cfa3
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2023-06-07 05:49:54 -0700

    Don't override user provided compile definitions (#145)
    * Don't override user provided compile definitions
    
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>
    (cherry picked from commit 454a7bc7d9910c5d33f682935b248cf7ec5af80e)

--- rosidl_typesupport_cpp/cmake/rosidl_typesupport_cpp_generate_interfaces.cmake
+++ rosidl_typesupport_cpp/cmake/rosidl_typesupport_cpp_generate_interfaces.cmake
@@ -92,11 +92,11 @@ endif()
 set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix}
   PROPERTIES
     DEFINE_SYMBOL "ROSIDL_TYPESUPPORT_CPP_BUILDING_DLL"
-    CXX_STANDARD 14)
+    CXX_STANDARD 17)
 
 if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix}
-    PROPERTIES COMPILE_OPTIONS -Wall -Wextra -Wpedantic)
+  target_compile_options(${rosidl_generate_interfaces_TARGET}${_target_suffix}
+    PRIVATE -Wall -Wextra -Wpedantic)
 endif()
 
 # if only a single typesupport is used this package will directly reference it
